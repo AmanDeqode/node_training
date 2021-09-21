540d0ff37e4c601e54b3f6b5a66e11bd718a6505
Aman Chaudhary <achoudhary@deqode.com>
2021-09-21 11:22:23 +0530

    Implement: logics for various usersRoute requests

---
+++ controller/usersController.js
@@ -0,0 +1,54 @@
+import fs from 'fs';
+import path from 'path';
+import { validationResult } from 'express-validator';
+
+import users from '../views/users.json';
+
+function saveUser(newUser) {
+  const totalUsers = users.length;
+
+  newUser.id = totalUsers + 1;
+
+  users.push(newUser);
+
+  fs.writeFile(
+    path.join(__dirname, '..', 'views/users.json'),
+    JSON.stringify(users),
+    (error) => {
+      if (error) {
+        return error.message;
+      }
+      console.log('Data has been stored into the file successfully');
+    }
+  );
+  return users;
+}
+exports.getallUsers = (req, res) =>
+  res.render('home', { users, msg: '', error: '' });
+exports.signup = (req, res) => res.render('signup', { error: '' });
+exports.addnewUser = (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.render('signup', { error: errors.array() });
+  }
+  const { email } = req.body;
+  const existingUser = users.filter((user) => user.email === email);
+
+  if (existingUser.length !== 0) {
+    return res.render('signup', { error: 'Email id already exists' });
+  }
+  saveUser(req.body);
+
+  return res.render('home', { users, msg: 'User has been successfully' });
+};
+exports.loginUser = (req, res) => {
+  const { email, password } = req.body;
+  const validUser = users.some(
+    (user) => user.email === email && user.password === password
+  );
+
+  if (validUser) {
+    return res.render('home', { users, msg: 'Loggedin Successfully' });
+  }
+  return res.render('login', { error: 'Invalid email id or password' });
+};
