155f190a94807c9c834b7350c0bd975538c5bc17
Aman Chaudhary <achoudhary@deqode.com>
2021-09-21 10:57:11 +0530

    Add: express middlewares

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
+
+import userRouter from './routes/usersRoute';
+
+dotenv.config();
+
+const { PORT, HOST } = process.env;
 
 const app = express();
 
-app.listen(3000, () => {
-    console.log(`Server is connected at http://localhost:3000`);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', userRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is connected at http://${HOST}:${PORT}`);
 });
