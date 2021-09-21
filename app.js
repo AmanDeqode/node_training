2afcfeb3595995094c2a93863ffc3269cb81718e
Aman Chaudhary <achoudhary@deqode.com>
2021-09-21 18:14:03 +0530

    Add:multer functionality to store file type data

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
-import path from 'path';
+import multer from 'multer';
 
 import userRouter from './routes/usersRoute';
 
 dotenv.config();
 
 const { PORT, HOST } = process.env;
+
+/*eslint-disable */
+const storage = multer.diskStorage({
+  destination: (req,file,cb) => {
+    cb(null, path.join(__dirname,'views/documents'));
+  },
+  filename: (req,file,cb) => {
+    console.log(file);
+    const {originalname} = file;
+    cb(null, new Date().toISOString().replace(/[\/\\:]/g, "_") + originalname)
+  }
+});
+/*eslint-enable */
 
 const app = express();
 
@@ -16,6 +30,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(multer({ storage }).single('docs'));
 
 app.use('/', userRouter);
 
