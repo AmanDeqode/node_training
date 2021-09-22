ce4ff0b43c3f92012100ed43a4e07c3ccbd4675c
Aman Chaudhary <achoudhary@deqode.com>
2021-09-22 18:26:14 +0530

    Add: ES6 class feature

--- app.js
+++ app.js
@@ -1,39 +1,44 @@
 import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
-import multer from 'multer';
 
-import userRouter from './routes/usersRoute';
+import routes from './routes/index';
 
 dotenv.config();
 
 const { PORT, HOST } = process.env;
 
-/*eslint-disable */
-const storage = multer.diskStorage({
-  destination: (req,file,cb) => {
-    cb(null, path.join(__dirname,'views/documents'));
-  },
-  filename: (req,file,cb) => {
-    console.log(file);
-    const {originalname} = file;
-    cb(null, new Date().toISOString().replace(/[\/\\:]/g, "_") + originalname)
-  }
-});
-/*eslint-enable */
-
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+class Server {
+  constructor() {
+    this.assigns();
+    this.middlewares();
+    this.routes();
+    this.start();
+  }
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(multer({ storage }).single('docs'));
+  assigns() {
+    app.set('view engine', 'ejs');
+    app.set('views', path.join(__dirname, 'views'));
+  }
 
-app.use('/', userRouter);
+  middlewares() {
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+    app.use(express.static(path.join(__dirname, 'public')));
+  }
 
-app.listen(PORT, () => {
-  console.log(`Server is connected at http://${HOST}:${PORT}`);
-});
+  routes() {
+    app.use(routes);
+  }
+
+  start() {
+    app.listen(PORT, () => {
+      console.log(`Server is connected at http://${HOST}:${PORT}`);
+    });
+  }
+}
+
+const listen = new Server();
+listen;
