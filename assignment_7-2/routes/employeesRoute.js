ff516716b01bbb941785a851f65b2406655cb64e
Aman Chaudhary <achoudhary@deqode.com>
2021-10-07 18:32:57 +0530

    Add: facebook and google authentication routes

--- assignment_7-2/routes/employeesRoute.js
+++ assignment_7-2/routes/employeesRoute.js
@@ -5,6 +5,7 @@ import multer from 'multer';
 import passport from 'passport';
 import Employee from '../controller/employeesController';
 import Credentials from '../controller/loginController';
+import '../config/passport';
 
 const router = express.Router();
 /* eslint-disable */
@@ -19,28 +20,39 @@ const storage = multer.diskStorage({
 }); /* eslint-enable */
 const uploads = multer({ storage });
 
-router.get(
-  '/employees',
-  Employee.checkNotAuthenticated,
-  Employee.getallEmployees
-);
+router.get('/employees', Credentials.isLoggedIn, Employee.getallEmployees);
 router.get('/signup', Employee.signup);
-router.get('/login', Credentials.checkAuthenticated, Credentials.loginPage);
+router.get('/login', Credentials.loginPage);
 router.get('/logout', Credentials.logout);
+router.get(
+  '/login/google',
+  passport.authenticate('google', { scope: ['email', 'profile'] })
+);
+router.get(
+  '/login/fb',
+  passport.authenticate('facebook', { scope: ['public_profile', 'email'] })
+);
+router.get(
+  '/callback/google',
+  passport.authenticate('google', {
+    successRedirect: '/employees',
+    failureRedirect: '/login',
+  })
+);
+router.get(
+  '/callback/fb',
+  passport.authenticate('facebook', {
+    successRedirect: '/employees',
+    failureRedirect: '/login',
+  })
+);
 router.post(
   '/employees',
   uploads.single('document'),
   Employee.validateData(),
   Employee.addEmployee
 );
-router.post(
-  '/login',
-  passport.authenticate('local', {
-    successRedirect: '/employees',
-    failureRedirect: '/login',
-  }),
-  Credentials.loginEmployee
-);
+router.post('/login', Credentials.loginEmployee);
 router.patch('/employees/:employeeId', Employee.editEmployee);
 router.delete('/employees/:employeeId', Employee.deleteEmployee);
 
