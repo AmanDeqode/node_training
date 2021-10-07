b8613c24a762746150e9b245e927eb216108e167
Aman Chaudhary <achoudhary@deqode.com>
2021-10-07 18:08:17 +0530

    Implement: logic for google and facebook passport authentication

--- assignment_7-2/config/passport.js
+++ assignment_7-2/config/passport.js
@@ -1,40 +1,90 @@
-import passportLocal from 'passport-local';
-import bcrypt from 'bcryptjs';
+import passport from 'passport';
+import googlePassport from 'passport-google-oauth20';
+import facebookPassport from 'passport-facebook';
+import dotenv from 'dotenv';
+
 import Employee from '../models/Employee';
 
-const LocalStrategy = passportLocal.Strategy;
+dotenv.config();
+const GoogleStrategy = googlePassport.Strategy;
+const FacebookStrategy = facebookPassport.Strategy;
 
-function initialize(passport) {
-  console.log('Initialized');
-
-  const authenticateEmployee = async (email, password, done) => {
-    try {
-      const employee = await Employee.findOne({
-        where: { email: email },
-      });
-      if (!employee) return done(null, false, { error: 'Invalid Email' });
-      const isMatch = await bcrypt.compare(password, employee.password);
-      if (!isMatch) return done(null, false, { error: 'Invalid Password' });
-      return done(null, employee);
-    } catch (error) {
-      done(error);
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.URL_GOOGLE,
+    },
+    async (accessToken, refreshToken, profile, cb) => {
+      try {
+        const employee = await Employee.findOne({
+          where: { id: parseInt(profile.id, 10) },
+        });
+        if (!employee) {
+          const employeewithEmail = await Employee.findOne({
+            where: { email: profile.emails[0].value },
+          });
+          if (!employeewithEmail) {
+            const newEmployee = await Employee.create({
+              username: profile.displayName,
+              email: profile.emails[0].value,
+              password: 'googleDefault',
+            });
+            return cb(null, newEmployee);
+          }
+          return cb(null, employeewithEmail);
+        }
+        return cb(null, employee);
+      } catch (error) {
+        throw new Error(error);
+      }
     }
-  };
-  passport.use(
-    new LocalStrategy(
-      {
-        usernameField: 'email',
-        passwordField: 'password',
-      },
-      authenticateEmployee
-    )
-  );
-  passport.serializeUser((employee, done) => done(null, employee.id));
-  passport.deserializeUser(async (id, done) => {
-    const employee = await Employee.findOne({
-      where: { id: id },
-    });
-    if (employee) return done(null, employee);
-  });
-}
-export default initialize;
+  )
+);
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+      callbackURL: process.env.URL_FB,
+      profileFields: ['id', 'email', 'name'],
+    },
+    async (accessToken, refreshToken, profile, cb) => {
+      try {
+        const employee = await Employee.findOne({
+          where: { id: parseInt(profile.id, 10) },
+        });
+        if (!employee) {
+          const employeewithEmail = await Employee.findOne({
+            where: { email: profile.emails[0].value },
+          });
+          if (!employeewithEmail) {
+            const newEmployee = await Employee.create({
+              username: `${profile.name.givenName} ${profile.name.familyName}`,
+              email: profile.emails[0].value,
+              password: 'facebookDefault',
+            });
+            return cb(null, newEmployee);
+          }
+          return cb(null, employeewithEmail);
+        }
+        return cb(null, employee);
+      } catch (error) {
+        throw new Error(error);
+      }
+    }
+  )
+);
+passport.serializeUser((employee, cb) => {
+  cb(null, employee.id);
+});
+passport.deserializeUser(async (id, cb) => {
+  try {
+    const employee = await Employee.findByPk(id);
+    cb(null, employee);
+  } catch (error) {
+    cb(error);
+    throw error;
+  }
+});
